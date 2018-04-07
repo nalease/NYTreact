327e1d3fe0e5cdc782cc44e7acd8359f0e0c5d82
Nalease Marrow <Naleasem@Naleases-MacBook-Pro.local>
2018-04-07 13:32:51 -0400

    Add Create

--- nytreact/controllers/articlecontroller.js
+++ nytreact/controllers/articlecontroller.js
@@ -12,6 +12,15 @@ module.exports = {
     db.Article.findById(req.params.id)
       .then(dbArticle => res.json(dbArticle))
       .catch(err => res.status(422).json(err));
-
-  }
+  },
+  create: function(req, res) {
+    const article = {
+      _id: req.body._id,
+      title: req.body.headline.main,
+      url: req.body.web_url
+    };
+    db.Article.create(article)
+      .then(dbArticle => res.json(dbArticle))
+      .catch(err => res.status(422).json(err));
+ 
 };
