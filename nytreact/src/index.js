93433dd77d673846ecd916cf17a0164e924d0acc
Nalease Marrow <Naleasem@Naleases-MacBook-Pro.local>
2018-04-07 13:28:24 -0400

    Add Controller

--- nytreact/src/index.js
+++ nytreact/src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+app.get('/', function (req, res) {
+  res.send('GET request to the homepage')
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
